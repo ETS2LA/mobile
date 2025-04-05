a8bb3c329c99c5f3fb6bdbee5225a0a91e8d3801
DylDev <dylanb92010@gmail.com>
2025-04-05 18:32:05 -0400

    Buildable

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ets2la.mobile',
+  appName: 'ETS2LA Mobile',
+  webDir: 'out'
+};
+
+export default config;
