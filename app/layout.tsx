404dc0d531d4c31007b9f3d73ccc441c9259891b
DylDev <dylanb92010@gmail.com>
2025-04-06 16:22:49 -0400

    Now completely works!

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import localFont from "next/font/local";
 import "./globals.css";
-import Script from "next/script";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -24,7 +23,7 @@ export default function RootLayout({ children, } : Readonly<{ children: React.Re
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="icon" href="/favicon.ico" />
             </head>
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-sidebarbg overflow-hidden`}>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-sidebar-primary overflow-hidden`}>
                 {children}
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
 "use client"
 
-import { useRouter } from "next/navigation";
+import "./globals.css";
+import { ClipLoader } from "react-spinners";
+import Image from "next/image";
+import logo from "@/assets/favicon.ico";
 
 export default function Home() {
-    const { push } = useRouter();
+    const imagesPreloaded = true
+
+    if (!imagesPreloaded) {
+        return (
+            <div className="flex flex-col items-center justify-center h-screen">
+                <p className="text-md text-muted-foreground mb-4">Preloading assets...</p>
+                <ClipLoader size={20} color="#717171"/>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>xdd</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <Image src={logo} alt="ETS2LA Logo" width={144} height={144} />
+            <h1 className="text-3xl font-bold text-white mt-5 mb-2">ETS2LA</h1>
+            <p className="text-md text-muted-foreground mb-6">Mobile</p>
+            <ClipLoader size={20} color="#717171"/>
         </div>
     );
 }
